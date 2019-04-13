a41324ea18d60cb4213d25544a7d1baee6b7b2ca
compassionate-scope-9333 <lexus2424@gmail.com>
2019-04-13 17:53:15 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  
+  if int % 3 == 0 && int % 5 == 0 
+      "FizzBuzz"
  
    elsif int % 5 == 0 # if the number int is divisible by 5
      "Buzz"
     
     # if number is divisible by 3 and 5 print "FizzBuzz"
     
-      if int % 3 == 0 # if the number int is divisible by 3
+     elsif int % 3 == 0 # if the number int is divisible by 3
     "Fizz"
      
    end
